2413840653cb8a6d9d8f05da5bd330cbd639ba00
n3iko <std150473@ac.eap.gr>
2024-03-13 20:11:56 +0200

    Javascript-Practice

---
+++ 8. Project 8-Expense-Tracker-Web/script.js
@@ -0,0 +1,84 @@
+const addExpenseBtn = document.querySelector(".add-expense-btn");
+const expenseList = document.querySelector(".expense-list");
+const totalExpenses = document.querySelector(".total-expenses h3");
+
+let expenses = [];
+let total = 0;
+
+
+function renderExpenses(){
+
+    let html = "";
+        expenses.forEach(expense => {
+
+            html += `
+                <div class="expense-item">
+                <div class="expense-item-description">${expense.description}</div>
+                <div class="expense-item-amount">€${expense.amount}</div>
+                <button class="delete-expense-btn">&times;</button>
+                </div>
+            `;
+        });
+
+        expenseList.innerHTML = html;
+    totalExpenses.innerText =  `Total Expenses: €${total.toFixed(2)}`; // Fix decimal precision        
+        const mongoose = require('mongoose');
+        mongoose.connect('mongodb://localhost:27017/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true });
+        
+        const { Schema } = mongoose;
+        
+        const expenseSchema = new Schema({
+            description: String,
+            amount: Number,
+            date: { type: Date, default: Date.now }
+        });
+        
+        const Expense = mongoose.model('Expense', expenseSchema);
+        
+
+}
+
+
+
+
+function addExpense(){
+
+    const description = prompt("Enter Expense Description:");
+    const amount = parseFloat(prompt("Enter Expense Amount"));
+
+    if (description && amount)
+    {
+        const expense = {
+            description:description,
+            amount:amount
+        };
+
+        expenses.push(expense);
+        total += amount;
+        renderExpenses();
+    }
+}
+
+addExpenseBtn.addEventListener("click", addExpense);
+
+
+function deleteExpense(index){
+
+    total -= expenses[index].amount;
+    expenses.splice(index,1)
+    renderExpenses();
+
+}
+
+expenseList.addEventListener("click",function(event){
+
+    if(event.target.classList.contains("delete-expense-btn")){
+
+        const index = Array.from(event.target.parentNode.parentNode.children)
+        .indexOf(event.target.parentNode);
+        
+        deleteExpense(index);
+    }
+
+
+});
